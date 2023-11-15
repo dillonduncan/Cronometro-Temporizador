6bdea7b6f55ae57335c9b8d4be181262aba202b0
Dillon Duncan <dillonduncan0607@gmail.com>
2023-11-14 22:22:11 -0500

    Temporizador Actulizado

--- app/src/main/java/com/example/cronometro/Cronometro_Activity.kt
+++ app/src/main/java/com/example/cronometro/Cronometro_Activity.kt
@@ -16,33 +16,34 @@ class Cronometro_Activity : AppCompatActivity() {
     }
 
     fun Timer() {
-        var minutos = 0
-        var segundos = 0
-        var milisegundos = 0
-        var timer = object : CountDownTimer(6000, 10) {
+        var minutos:Long = 0
+        var segundos:Long = 0
+        var milisegundos:Long = 0
+        var sec=0
+        var timer = object : CountDownTimer(1000, 1) {
             override fun onTick(millisUntilFinished: Long) {
-                milisegundos++
-                if (milisegundos > 99) {
+                milisegundos=millisUntilFinished/10
+                sec++
+                if (milisegundos > 98 && sec>1) {
                     segundos++
                     milisegundos = 0
                 }
-
                 if (segundos > 59) {
                     minutos++
                     segundos = 0
                 }
                 if (milisegundos < 10 && segundos < 10 && minutos < 10) {
-                    binding.txtTiempo.text = "0$minutos:0$segundos.0$milisegundos"
+                    binding.txtTiempo.text = "0$minutos : 0$segundos . 0$milisegundos"
                 } else if (minutos < 10 && segundos > 9 && milisegundos < 10) {
-                    binding.txtTiempo.text = "0$minutos:$segundos.0$milisegundos"
+                    binding.txtTiempo.text = "0$minutos : $segundos . 0$milisegundos"
                 } else if (minutos >9 && segundos > 9 && milisegundos < 10) {
-                    binding.txtTiempo.text = "0$minutos:0$segundos.$milisegundos"
+                    binding.txtTiempo.text = "0$minutos : 0$segundos . $milisegundos"
                 } else if (segundos < 10 && minutos < 10) {
-                    binding.txtTiempo.text = "0$minutos:0$segundos.$milisegundos"
+                    binding.txtTiempo.text = "0$minutos : 0$segundos . $milisegundos"
                 } else if (segundos < 10) {
-                    binding.txtTiempo.text = "$minutos:0$segundos.$milisegundos"
+                    binding.txtTiempo.text = "$minutos : 0$segundos . $milisegundos"
                 } else if (minutos < 10) {
-                    binding.txtTiempo.text = "0$minutos:$segundos.$milisegundos"
+                    binding.txtTiempo.text = "0$minutos : $segundos . $milisegundos"
                 }
             }
 
@@ -63,10 +64,11 @@ class Cronometro_Activity : AppCompatActivity() {
         }
         binding.btnRestart.setOnClickListener {
             milisegundos = 0
+            sec=0
             segundos = 0
             minutos = 0
             timer.cancel()
-            binding.txtTiempo.text = "0$minutos:0$segundos.0$milisegundos"
+            binding.txtTiempo.text = "0$minutos : 0$segundos . 0$milisegundos"
             binding.btnStarStop.text = "Iniciar"
             binding.btnRestart.isEnabled = false
         }
